4175ebacb3ff9195618886104fe59a7330682db9
softsunlight <1104725483@qq.com>
2021-11-13 10:20:34 +0800

    修改项目名称

---
+++ SoftSunlightIM.WebApi.Models/Enum/DeviceTypeEnum.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyQQ.WebApi.Models
+{
+    /// <summary>
+    /// 设备类型
+    /// </summary>
+    public enum DeviceTypeEnum
+    {
+        Pc = 1,
+        Web = 2,
+        Mobile = 3
+    }
+}
